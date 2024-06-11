7df64d909dd2deeecebdd722981da4757b0a0ec8
IliaLataria01 <latariailia6@gmail.com>
2024-06-11 14:15:21 +0400

    Finished Configuration of notification-alert Service and comes actual implementation, also added OpenAI Dependency just for test

---
+++ notalert-service/src/test/java/com/demo/folder/notalertservice/NotalertServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.folder.notalertservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotalertServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
